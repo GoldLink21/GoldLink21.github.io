f9694741a94bcb7f3b705719b8e66895a2e3d5ce
Christian Holtsclaw <ultragamerh21@gmail.com>
2018-07-12 11:11:43 -0400

    Updated stuff

--- fade-in.js
+++ fade-in.js
@@ -1,10 +1,19 @@
 function typeWriter(id,delay){
     var fade=document.getElementById(id);
     var text=fade.textContent;
-    fade.innerHTML='';
-    for (let i=0; i<text.length;i++) {
-        setTimeout( function timer(){
+    fade.textContent='';
+    for(let i=0;i<text.length;i++){
+        setTimeout(function timer(){
+            if(text.charAt(i)!=='^'){
                 fade.innerHTML+=text.charAt(i);
-        }, i*delay );
+            }else{
+                fade.innerHTML+='</br>';
+            }
+        },i*delay);
     }
+}
+
+function addTag(id,tagName,textInside){
+    var ele=document.getElementById(id);
+    ele.innerHTML+='<'+tagName+'>'+textInside+'</'+tagName+'>';
 }
---
+++ header.js
@@ -0,0 +1,3 @@
+function getScrollPosition(){
+    return document.body.scrollTop;
+}
