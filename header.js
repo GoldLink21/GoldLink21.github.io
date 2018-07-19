5b3e31218502c7106096ef6dc133a2119ce9e79d
Christian Holtsclaw <ultragamerh21@gmail.com>
2018-07-19 13:13:24 -0400

    Added a lot

--- header.js
+++ header.js
@@ -1,3 +1,12 @@
-function getScrollPosition(){
-    return document.body.scrollTop;
+const scrollLimit=20;
+window.onscroll=()=>{
+    /*
+    var h1=document.querySelector('h1');
+    if(document.body.scrollTop>scrollLimit||document.documentElement.scrollTop>scrollLimit){
+        h1.classList.remove('top')
+        h1.classList.add('down');
+    }else{
+        h1.classList.add('top')
+        h1.classList.down('down');
+    }*/
 }
